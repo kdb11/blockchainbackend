f7987bce8b15a47aaefd1bbd5b7839ae72367c72
kdb11 <112870790+kdb11@users.noreply.github.com>
2023-08-25 22:01:18 +0200

    chainblock

---
+++ chainBlock.js
@@ -0,0 +1,44 @@
+let hash = require ('object-hash');
+
+class ChainBlock {
+
+    constructor() {
+
+        this.chain = [];
+
+        this.currerntTransactions = [];
+
+    }
+
+    addNewBlock(prevHash) {
+        let block = {
+            index: this.chain.length + 1,
+            timestamp: Date.now(),
+            transactions: this.currerntTransactions,
+            /* hash: null, */ 
+            prevHash: prevHash,
+        };
+
+        this.hash = hash(block);
+
+        this.chain.push(block);
+        this.currerntTransactions = [];
+        return block;
+
+    }
+
+    addNewTransaction(sender, recipient, amount) {
+        this.currerntTransactions.push({ sender, recipient, amount })
+    }
+
+    isEmpty() {
+        return this.chain.length == 0;
+    }
+
+    lastBlock() {
+        return this.chain.slice(-1)[0];
+    }
+
+}
+
+module.exports = ChainBlock;
