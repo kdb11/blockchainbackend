2ef3837f4fd65ba819454ff5bcfa6c5c02608c71
kdb11 <112870790+kdb11@users.noreply.github.com>
2023-08-27 22:28:28 +0200

    mine with api

--- api.js
+++ api.js
@@ -1,8 +1,15 @@
 const express = require ('express');
+let ProofOfWork = require ('./proofOfWork');
+let proofOfWork = new ProofOfWork();
+let Hash = require ('./hash');
+let hashedvalue = new Hash();
 let BlockChain = require("./blockChain");
+const voteChain = new BlockChain();
+
+console.log(proofOfWork, hashedvalue);
+
 const app = express();
 
-const voteChain = new BlockChain();
 app.use(express.json());
 
 app.get('/api/blockchain', (req, res) => {
@@ -12,6 +19,25 @@ app.post('/api/vote', (req, res) => {
     const index = voteChain.addNewVote(req.body.voter, req.body.candidate, req.body.voteToken);
     res.status(201).json({ sucess: true, data: `block index: ${index}` })
 });
-app.get('/api/mine', (req, res) => { });
+app.get('/api/mine', (req, res) => { 
+    const prevBlock = voteChain.lastBlock();
+    const prevBlockHash = prevBlock.hash;
+
+    const data = {
+        data: voteChain.pendingVotes,
+        index: prevBlock.index + 1,
+    };
+
+    const nonce = proofOfWork.proofOfWork(prevBlockHash, data);
+
+    const hash = hashedvalue.createHash(prevBlockHash, data, nonce);
+
+    const block = voteChain.addNewBlock(prevBlockHash, nonce, hash);
+
+    res.status(200).json({
+        sucess: true,
+        data: block,
+    });
+});
 
 app.listen(3000, () => console.log('Server  on port 3000'));
